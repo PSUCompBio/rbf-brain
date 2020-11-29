caed6d9d15a4b08fe990ea34e7694b95ce3207f4
Reuben Kraft <reuben.kraft@gmail.com>
2020-11-29 15:17:12 -0500

    Update pass_check.sh

--- pass_check.sh
+++ pass_check.sh
@@ -7,8 +7,13 @@ target_file_size=1537589
 
 # Add file 2 and the target file size below
 repo2=rbf-brain-inp-creation
-file2=coarse_mesh_morphed.inp
-target_file_size2=1691557
+file2=coarse_mesh_morphed-v2.inp
+target_file_size2=1761823
+
+# Add file 3 and the target file size below
+repo3=rbf-vtk-to-vtu-conversion
+file3=converted_vtk.vtu
+target_file_size3=1977419
 
 # You shoud not have to modify below
 #
@@ -19,6 +24,10 @@ echo Target File Size = "$target_file_size"
 myfilesize2=$(wc -c <"$file2")
 echo Acutal File2 Size = "$myfilesize2"
 echo Target File2 Size = "$target_file_size2"
+
+myfilesize3=$(wc -c <"$file3")
+echo Acutal File3 Size = "$myfilesize3"
+echo Target File3 Size = "$target_file_size3"
 
 if [ $myfilesize -ge $target_file_size ];then
         echo Passed!
@@ -42,5 +51,18 @@ else
         echo "Failed!" >> ~/$repo2.FAILED
         echo "Acutal File2 Size = "$myfilesize2" " >> ~/$repo2.FAILED
         echo "Target File2 Size = "$target_file_size2" " >> ~/$repo2.FAILED
+
+fi
+
+if [ $myfilesize3 -ge $target_file_size3 ];then
+        echo Passed!
+        echo "Passed" >> ~/$repo3.PASSED
+        echo "Acutal File2 Size = "$myfilesize3" " >> ~/$repo3.PASSED
+        echo "Target File2 Size = "$target_file_size3" " >> ~/$repo3.PASSED
+else
+        echo Failed!
+        echo "Failed!" >> ~/$repo3.FAILED
+        echo "Acutal File3 Size = "$myfilesize3" " >> ~/$repo3.FAILED
+        echo "Target File3 Size = "$target_file_size3" " >> ~/$repo3.FAILED
 
 fi
